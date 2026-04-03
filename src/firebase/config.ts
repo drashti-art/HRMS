44b8531ac2d534e30aa21627e3008bb16d90de33
Drashti Khanpara <drashti@aekads.com>
2026-04-03 03:59:44 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3010074024-b45de",
+  "appId": "1:70005791114:web:fe48f1fb4e1251d4fd9073",
+  "apiKey": "AIzaSyAeDG7RgzqiAlawWZg7rN8ZNtgdwnsLNPY",
+  "authDomain": "studio-3010074024-b45de.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "70005791114"
+};
